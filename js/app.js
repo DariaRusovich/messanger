bc67410723a91590e786f5a0218c9e447609fb46
DariaRusovich <rusovichdasha41@gmail.com>
2021-06-14 17:03:21 +0300

    new-projest-messenger

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const cardData = JSON.parse(DATA) 
+
+
+
+
+
+
+
+
+
+
+// "id": 1,
+//     "phone": "+63 (924) 979-2252",
+//     "name": "Guss Marvelley",
+//     "text": "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
+//     "avatar": "https://robohash.org/repellendusimpeditnisi.png?size=50x50&set=set1",
+//     "date": "1609595510000",
+//     "seen": false
