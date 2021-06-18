931f2fcefc3ccd9fb0b802b271d6b2e0488ed2f5
DariaRusovich <72560988+DariaRusovich@users.noreply.github.com>
2021-06-18 15:58:55 +0300

    sorting-items-remove-messages

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-const cardsMessegeData = JSON.parse(DATA); //JSON глобальный обьект
+let cardsMessegeData = JSON.parse(DATA); //JSON глобальный обьект
 const cardMessegeList = document.getElementById("cardMessegeList");
 const reloadBtn = document.getElementById("reloadBtn");
+const allCount = document.getElementById('allCount')
+const unreadCount = document.getElementById('unreadCount')
+
 //console.log(DATA);
 //console.log(cardData);
 const timeFormatter = new Intl.DateTimeFormat(undefined, {
@@ -14,9 +17,23 @@ const dateFormatter = new Intl.DateTimeFormat(undefined, {
 });
 
 reloadBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  window.location.reload();
+  cardsMessegeData = JSON.parse(DATA)
+  renderMessegeCards(createCardsHTML(cardsMessegeData), cardMessegeList);
 });
+cardMessegeList.addEventListener('click', event => {
+  const messageText = event.target.closest('.text')
+  if (messageText) {
+    const messageId = messageText.closest('.card-wrapper').dataset.id
+    const messageIdx = cardsMessegeData.findIndex(message => message.id == messageId)
+    const message = cardsMessegeData[messageIdx]
+    if (message.seen) {
+      cardsMessegeData.splice(messageIdx, 1)
+    } else{
+      message.seen = true
+    }
+    renderMessegeCards(createCardsHTML(cardsMessegeData), cardMessegeList);
+  }
+})
 
 // "id": 1,
 // "phone": "+63 (924) 979-2252",
@@ -33,26 +50,26 @@ reloadBtn.addEventListener("click", (event) => {
 renderMessegeCards(createCardsHTML(cardsMessegeData), cardMessegeList);
 
 function createCardsHTML(cardsArray) {
+  allCount.textContent = cardsArray.length
+  unreadCount.textContent = cardsArray.filter(message => !message.seen).length
   let cardsHTML = "";
+  cardsArray.sort((a, b) => {
+    return a.seen - b.seen || b.date - a.date;
+  });
   cardsArray.forEach((card) => {
     cardsHTML += createCard(card); //в каждую итерацию в cardsHTML каждый раз добавлять строку возвращенную функцией
-    console.log(cardsArray);
-    cardsArray.sort((a, b) => {
-      return a.seen - b.seen || a.date - b.date;
-    });
   });
   return cardsHTML;
 }
 
 //renderMessegeCards получает то, что вставлять(cardHTML) и получает элем куда вставлять (cardListElem)
 function renderMessegeCards(cardHTML, cardListElem) {
-  cardListElem.insertAdjacentHTML("beforeend", cardHTML); //insertAdjacentHTML метод у любого узла, принимает два парам, куда вставлять и строку с html
+  cardListElem.innerHTML = cardHTML; //insertAdjacentHTML метод у любого узла, принимает два парам, куда вставлять и строку с html
 }
 
-const dateObj = new Date(cardsMessegeData.date);
 
 function createCard(messegeData) {
-  return `<div class = "card-wrapper row text-center pt-4 pb-4 align-items-start">
+  return `<div class = "card-wrapper row text-center pt-4 pb-4 align-items-start ${!messegeData.seen ? 'unseen' : ''}" data-id="${messegeData.id}">
     <div
     class="user col-3 d-flex align-items-end"
     >
@@ -86,3 +103,5 @@ function createCard(messegeData) {
 //a = [false, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, false, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
 //a.sort((x, y) => Number(x) - Number(y));
 //console.log(a);
+
+
